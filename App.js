59a75fbfd0287637503ea76d5a46b38bff07dfed
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-27 00:42:59 +0200

    practice directives

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 Vue.createApp({
     data: () => ({
         myHtml: '<h1>Vue 3 App</h1>',
-        title: 'I am a title'
+        title: 'I am a title',
+        person: {
+            firstName: 'Andrii',
+            lastName: 'Pyrih',
+            age: 27
+        },
+        items: [1, 2, 3, 4, 5, 6, 7]
+
     })
 }).mount('#app')
